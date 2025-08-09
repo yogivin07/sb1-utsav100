38c43e446310b99a3fb5914794497b59f34377b9
yogivin07 <yogendra.pawar@gmail.com>
2025-08-09 11:50:25 +0530

    test_0908

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -8,8 +8,8 @@ export default function Contact() {
         खटाव बिल्डिंग सार्वजनिक उत्सव मंडळ, वर्ष १००.
     
         </p>
-          yogendra.pawar@skenergy.com TEST
-          TEST
+          yogendra.pawar@skenergy.com
+    
         
           
       
